4ad4bd683b05dbeef28e4df1943c73bce6a9cf2c
GlennVinck <vinck_glenn@hotmail.com>
2023-11-28 15:17:52 +0100

    feat: setup first basic routes GET POST PUT DELETE with dummy data

--- routes/api/v1/shoes.js
+++ routes/api/v1/shoes.js
@@ -4,9 +4,45 @@ const router = express.Router();
 router.get("/", function (req, res) {
   res.json({
     status: "success",
+    message: "All shoes retrieved",
     data: {
       shoes: [],
     },
+  });
+});
+
+router.get("/:id", function (req, res) {
+  let id = req.params.id;
+  res.json({
+    status: "success",
+    message: `Shoe with id ${id} retrieved`,
+    data: {
+      shoes: [],
+    },
+  });
+});
+
+router.post("/", function (req, res) {
+  res.json({
+    status: "success",
+    message: "New shoe created",
+    data: {
+      shoes: [],
+    },
+  });
+});
+
+router.delete("/:id", function (req, res) {
+  res.json({
+    status: "success",
+    message: `Shoe with id ${req.params.id} deleted`,
+  });
+});
+
+router.put("/:id", function (req, res) {
+  res.json({
+    status: "success",
+    message: `Shoe with id ${req.params.id} updated`,
   });
 });
 
