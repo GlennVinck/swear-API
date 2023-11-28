d03e786f1929630cf6ae523f98b89e51b9668234
GlennVinck <vinck_glenn@hotmail.com>
2023-11-28 17:41:11 +0100

    feat: create order and send to mongodb

--- controllers/api/v1/orders.js
+++ controllers/api/v1/orders.js
@@ -1,3 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const orderSchema = new Schema({
+  customerName: {
+    type: String,
+    required: true,
+  },
+  shoeSize: {
+    type: Number,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    default: 1,
+  },
+  orderDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const Order = mongoose.model("Order", orderSchema);
+
 const getAllOrders = (req, res) => {
   res.json({
     status: "success",
@@ -19,14 +53,35 @@ const getSingleOrder = (req, res) => {
   });
 };
 
-const createOrder = (req, res) => {
-  res.json({
-    status: "success",
-    message: "New order created",
-    data: {
-      shoes: [],
-    },
-  });
+const createOrder = async (req, res) => {
+  try {
+    const order = new Order({
+      customerName: req.body.customerName,
+      shoeSize: req.body.shoeSize,
+      color: req.body.color,
+      price: req.body.price,
+      image: req.body.image,
+      quantity: req.body.quantity,
+      orderDate: req.body.orderDate,
+    });
+
+    const savedOrder = await order.save();
+
+    res.json({
+      status: "success",
+      message: "New order created",
+      data: {
+        order: savedOrder,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error",
+      data: null,
+    });
+  }
 };
 
 const deleteOrder = (req, res) => {
@@ -48,3 +103,4 @@ module.exports.createOrder = createOrder;
 module.exports.getSingleOrder = getSingleOrder;
 module.exports.deleteOrder = deleteOrder;
 module.exports.updateOrder = updateOrder;
+module.exports.Order = Order;
